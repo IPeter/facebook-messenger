9a2b7d5f9e4d07d9609a8c9401e40976495fac20
IPeter <illes.peter.deb@gmail.com>
2018-05-02 17:52:14 +0200

    support custom http clients

--- welcome.go
+++ welcome.go
@@ -62,8 +62,7 @@ func (msng *Messenger) setWelcome(m interface{}) error {
 	req, err := http.NewRequest("POST", msng.pageURL, bytes.NewBuffer(s))
 	req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := msng.HttpClient.Do(req)
 	if err != nil {
 		return err
 	}
